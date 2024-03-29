771ca9d2e6119cb9a1dad3df80a93ef4998cbfbb
Ткачёв Даниил <tdvwork696@gmail.com>
2024-03-29 12:21:08 +0300

    All_2

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from rest_framework import routers
 
 from cats.views import AchievementViewSet, CatViewSet
+
 
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
